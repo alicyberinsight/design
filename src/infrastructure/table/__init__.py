d14daa1cae085673a0f14f7936ce5b61f86964fa
alicyberinsight <ali.akbar@cyber-insight.de>
2025-11-24 16:39:41 +0100

    feat: Add initial table models for Blog and User with base class

---
+++ src/infrastructure/table/__init__.py
@@ -0,0 +1,9 @@
+from .base import Base
+from .blog_table import BlogTable
+from .user_table import UserTable
+
+__all__ = [
+    "Base",
+    "BlogTable",
+    "UserTable",
+]
---
+++ src/infrastructure/table/user_table.py
@@ -0,0 +1,29 @@
+from sqlalchemy import (
+    Integer,
+    String,
+)
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+)
+
+from src.infrastructure.table import Base
+
+
+class UserTable(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        autoincrement=True,
+    )
+    email: Mapped[str] = mapped_column(
+        String(255),
+        unique=True,
+        nullable=False,
+    )
+    full_name: Mapped[str] = mapped_column(
+        String(255),
+        nullable=False,
+    )
